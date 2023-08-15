bfc23b7638da3efc6fe3d89e9d4f31fc8a5cf306
Folarin <eribakefolarin@gmail.com>
2023-08-15 02:52:59 -0700

    hellothere

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times in lowercase
+ */
+void print_alphabet_x10(void)
+{
+    char ch;
+    int i;
+
+    i = 0;
+
+    while (i < 10)
+    {
+        ch = 'a';
+
+        while (ch <= 'z')
+        {
+            _putchar(ch);
+            ch++;
+        }
+
+        _putchar('\n');
+        i++;
+    }
+}
+
