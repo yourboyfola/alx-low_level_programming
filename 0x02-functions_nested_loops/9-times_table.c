ee99b67698826132e2337f6336c16d68cae0e8f6
Folarin <eribakefolarin@gmail.com>
2023-08-15 07:07:06 -0700

    Hello

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,36 +3,36 @@
  */
 void times_table(void)
 {
-	int i, j, k;
+    int i, j, k;
 
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			k = j * i;
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 10; j++)
+        {
+            k = j * i;
 
-			if (j == 0)
-			{
-				_putchar(k + '0');
-			}
+            if (j == 0)
+            {
+                putchar(k + '0');
+            }
 
-			if (k < 10 && j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			}
-			else if (k >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			}
-		}
+            if (k < 10 && j != 0)
+            {
+                putchar(',');
+                putchar(' ');
+                putchar(' ');
+                putchar(k + '0');
+            }
+            else if (k >= 10)
+            {
+                putchar(',');
+                putchar(' ');
+                putchar((k / 10) + '0');
+                putchar((k % 10) + '0');
+            }
+        }
 
-		_putchar('\n');
-	}
+        putchar('\n');
+    }
 }
 
