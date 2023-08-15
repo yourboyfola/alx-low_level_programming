272f1db30df6ba946406e5a656da69dd1abfd4db
Folarin <eribakefolarin@gmail.com>
2023-08-15 06:40:20 -0700

    Hello

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -1,11 +1,33 @@
-#include <stdio.h>
+#include "main.h"
 
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    int num = 42;
+    int r;
 
-    printf("Sign of %d is %d\n", num, print_sign(num));
-
-    return 0;
+    r = print_sign(98);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(0);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(0xff);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    r = print_sign(-1);
+    _putchar(',');
+    _putchar(' ');
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
 }
-
